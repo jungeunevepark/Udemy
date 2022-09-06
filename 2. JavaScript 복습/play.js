82d64da491d8605958c23bde6c7a37aaff686d35
jungeunevepark <evecomcom98@naver.com>
2022-09-06 23:13:03 +0900

    화살표 함수 더 짧게

--- 2. JavaScript 복습/play.js
+++ 2. JavaScript 복습/play.js
@@ -4,11 +4,10 @@ const hasHobbies = true;
 // var은 새로운 변수를 생성하는 키워드, 오래됨 -> let사용
 // const -> 변경 불가 / let -> 변경가능
 
-name = 'Maximilian';                      // error발생
 age = 30;
 
-const summarizeUser = function (userName, userAge, userHasHobby){              // 오른쪽을 무명함수라고 부름
-    return (                                                                   // 기명상수에 저장하므로써 기명함수처럼 사용할 수 있음
+const summarizeUser = (userName, userAge, userHasHobby) => {   // 강의 내에서는 화살표 함수만 사용할 것
+    return (                                                   // function body
         'Name is ' + 
         userName + 
         ', age is ' + 
@@ -18,5 +17,10 @@ const summarizeUser = function (userName, userAge, userHasHobby){              /
     );
 } // 굳이 변수로 받지 않아도 되지만 바람직한 형태임(외부에서 변수를 받기 때문)
 
+const add = (a, b) => {
+    return a + b;
+}
+// return 문 하나만 있는 화살표 함수라면 중괄호와 return 생략 가능
 
+console.log(add(1, 2));
 console.log(summarizeUser(name, age, hasHobbies));
