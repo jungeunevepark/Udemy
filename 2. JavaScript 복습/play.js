4ff01b13a1814ae155ce18672c003cc34ac60316
jungeunevepark <evecomcom98@naver.com>
2022-09-07 00:35:43 +0900

    promise

--- 2. JavaScript 복습/play.js
+++ 2. JavaScript 복습/play.js
@@ -1,40 +1,25 @@
-const person = {
-    name: 'MAX',
-    age: 29,
-    greet(){
-        console.log('Hi, I am ' + this.name)
-    }
+const fetchDate = () => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve('Done!');
+        }, 1500);
+    });
+    return promise;
 };
 
-const printName = ({ name }) => {
-    console.log(name);
-}
+setTimeout(() => {
+    console.log('Timer is done!');  // 콜백함수
+    fetchDate().then(done => {
+        console.log(text);
+        //프로미스의 장점
+        fetchDate().then
+    });
+}, 2000);
+// 이렇게 중첩화된 비동기화 함수가 존재하는 경우 콜백 시점에서 점점 깊게 들어가게 됨
 
-printName(person);
+//비동기화 코드로 즉시 실행되거나 끝나지 않고 시간이 소요됨
 
-const { name, age } = person;
-console.log(name, age);
-
-
-// const copiedPerson = {...person};
-// console.log(copiedPerson);
-
-const hobbies = ['Sports', 'Cooking']; // text array
-const [hobby1, hobby2] = hobbies;
-console.log(hobby1, hobby2);
-
-// // for (let hobby of hobbies) {
-// //     console.log(hobby);
-// // }
-// // console.log(hobbies.map(hobby => 'Hobby: ' + hobby));
-
-// // const copiesArray = hobbies.slice();     // 배열을 복사함
-// // const copiesArray = [hobbies];              // 중첩 배열
-// const copiesArray = [...hobbies];
-// console.log(copiesArray);
-
-// const toArray = (...args) => {
-//     return args;
-// };
-
-// console.log(toArray(1, 2, 3, 4));
+console.log('Hello!');
+console.log('Hi!')
+//코드 실행이 종료될 때까지는 이를 멈추지 않음.
+//모든 동기화 코드를 실행하고 비동기화 코드를 실행함.
