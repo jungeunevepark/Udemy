976f06639a7674ab68bb6b51a07692a7c0c70e2f
jungeunevepark <evecomcom98@naver.com>
2022-09-06 23:08:22 +0900

    무명함수

--- 2. JavaScript 복습/play.js
+++ 2. JavaScript 복습/play.js
@@ -1,9 +1,14 @@
-var name = 'MAX';               // string 변수
-var age = 29;
-var hasHobbies = true;
+const name = 'MAX';               // string 변수
+let age = 29;
+const hasHobbies = true;          
+// var은 새로운 변수를 생성하는 키워드, 오래됨 -> let사용
+// const -> 변경 불가 / let -> 변경가능
 
-function summarizeUser(userName, userAge, userHasHobby){
-    return (
+name = 'Maximilian';                      // error발생
+age = 30;
+
+const summarizeUser = function (userName, userAge, userHasHobby){              // 오른쪽을 무명함수라고 부름
+    return (                                                                   // 기명상수에 저장하므로써 기명함수처럼 사용할 수 있음
         'Name is ' + 
         userName + 
         ', age is ' + 
